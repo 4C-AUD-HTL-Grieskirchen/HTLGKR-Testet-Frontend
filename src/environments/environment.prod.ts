2a7b32fc991c1f97329ac90632c9cb90f017d95a
Michael Wiesinger <michw2014@gmail.com>
2021-04-04 23:11:06 +0200

    Updated theme

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-    production: true
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyB04YQ9Xv7ZHyMjUNvu3iRwRm-rxrXXMsg',
+        authDomain: 'htlgkr-testet.firebaseapp.com',
+        databaseURL: 'https://htlgkr-testet-default-rtdb.firebaseio.com',
+        projectId: 'htlgkr-testet',
+        storageBucket: 'htlgkr-testet.appspot.com',
+        messagingSenderId: '763205028685',
+        appId: '1:763205028685:web:7887501a8a5ed3b9ed4504',
+        measurementId: 'G-WCD5F7SQNN'
+    }
 };
